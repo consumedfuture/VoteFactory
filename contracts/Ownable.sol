32f9f335e640691f67908eb6a1844e11505e29b7
Александр Лычагов <consumedfuture@gmail.com>
2018-07-13 15:38:56 +0500

    refactoring and new functions

---
+++ contracts/Ownable.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+
+contract Ownable  {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        owner = _newOwner;
+    }
+}
--- contracts/VoteFactory.sol
+++ contracts/VoteFactory.sol
@@ -1,32 +1,63 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
 
-contract VoteFactory {
-    
-    address owner;
+import "./Ownable.sol";
+
+contract VoteFactory is Ownable {
+
+    enum State {
+        Initial, 
+        Started, 
+        Stopped
+    }
 
     struct Vote {
         string question;
         string[] answers;
+        State state;
     }
     
     Vote[] public votes;
     mapping (uint => address) voteToOwner;
     
-    constructor () public {
-        owner = msg.sender;
-    }
-    
     modifier onlyVoteOwner(uint _voteId) {
         require(voteToOwner[_voteId] == msg.sender);
         _;
     }    
+
+    modifier voteStateIsInitial(uint _voteId) {
+        require(votes[_voteId].state == State.Initial);
+        _;
+    }
+
+    modifier voteStateIsStarted(uint _voteId) {
+        require(votes[_voteId].state == State.Started);
+        _;
+    }
+
+    modifier voteStateIsStopped(uint _voteId) {
+        require(votes[_voteId].state == State.Stopped);
+        _;
+    }
+
+    modifier voteStateIsInitialOrStopped(uint _voteId) {
+        require((votes[_voteId].state == State.Initial) || (votes[_voteId].state == State.Stopped));
+        _;
+    }
     
     function createVote(string _question) public {
-        uint VoteId = votes.push(Vote(_question, new string[](0))) - 1;
+        uint VoteId = votes.push(Vote(_question, new string[](0), State.Initial)) - 1;
         voteToOwner[VoteId] = msg.sender;
     }
     
-    function addAnswer(uint _voteId, string _answer) onlyVoteOwner(_voteId) public {
+    function addAnswer(uint _voteId, string _answer) onlyVoteOwner(_voteId) voteStateIsInitial(_voteId) public {
         votes[_voteId].answers.push(_answer);
+    }
+
+    function startVote(uint _voteId) onlyVoteOwner(_voteId) voteStateIsInitialOrStopped(_voteId) public {
+        votes[_voteId].state = State.Started;
+    }
+
+    function stopVote(uint _voteId) onlyVoteOwner(_voteId) voteStateIsStopped(_voteId) public {
+        votes[_voteId].state = State.Stopped;
     }
 }
